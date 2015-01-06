d320fecf56e24de423ccbb568d2c5a77887038b1
Christfoffer Holmgren <ch222kv@student.lnu.se>
2015-01-06 13:16:21 +0100

    Converted the router.get('/') route to a promise function instead. Makes it much more manageable, but it's a little difficult to create the actual promises. It's not nice when you get other errors, because they do not get reported as they should, they do not explode the stack.
    - Does all the functionality of the previous function, with way less code.

--- application/application/routes/index.js
+++ application/application/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var spotify = require('../settings/settings').Spotify;
-var crypto = require("crypto");
-
+var crypto = require('crypto');
+var Q = require('q');
 
 var redis = require('redis'),
     client = redis.createClient();
@@ -11,19 +11,50 @@ var redis = require('redis'),
 /* GET home page. */
 router.get('/', function (req, res) {
     console.log(req.cookies);
-    if(req.cookies.username && req.cookies.hmac){
-        client.get(req.cookies.username, function(error_username, result_username){
-            if(error_username){
+    if (req.cookies.username && req.cookies.hmac) {
+        Q.ninvoke(client, "get", [req.cookies.username]).then(
+            function (result_username) {
+                console.log(result_username);
+                var json = JSON.parse(result_username);
+                var hmac = crypto.createHash("md5").update(json.session.name + json.session.key, "utf8").digest("hex");
+                console.log(req.cookies);
+                console.log(hmac);
+
+                if (hmac === req.cookies.hmac) {
+                    return Q.ninvoke(client, "lrange", "latestsearches", -5, - 1);
+                } else {
+                    throw new Error("hmac not matching");
+                }
+            }
+        ).then(function (result) {
+                res.render('index', {
+                    title: 'Music comparer',
+                    latestsearches: result,
+                    authenticated: true,
+                    username: req.cookies.username
+                });
+            }).catch(function (error) {
+                console.log(error);
                 res.cookie("username", "", {expires: new Date(1), httpOnly: true});
                 res.cookie("hmac", "", {expires: new Date(1), httpOnly: true});
                 res.render("error", {error: new Error("Your cookies seems to be wrong, try to login again.")});
+            });
+        /*client.get(req.cookies.username, function (error_username, result_username) {
+            if (error_username) {
+
             } else {
+
                 var json = JSON.parse(result_username);
-                var hmac = crypto.createHash("md5").update(json.session.name+json.session.key, "utf8").digest("hex");
-                if(hmac === req.cookies.hmac){
-                    client.lrange("latestsearches", -5, -1, function(error, result){
+                var hmac = crypto.createHash("md5").update(json.session.name + json.session.key, "utf8").digest("hex");
+                if (hmac === req.cookies.hmac) {
+                    client.lrange("latestsearches", -5, -1, function (error, result) {
                         console.log(arguments);
-                        res.render('index', {title: 'Music comparer', latestsearches: result, authenticated: true, username: req.cookies.username});
+                        res.render('index', {
+                            title: 'Music comparer',
+                            latestsearches: result,
+                            authenticated: true,
+                            username: req.cookies.username
+                        });
                     });
                 }
                 else {
@@ -31,25 +62,19 @@ router.get('/', function (req, res) {
                     res.cookie("hmac", "", {expires: new Date(1), httpOnly: true});
                     res.render("error", {error: new Error("Your cookies seems to be wrong, try to login again.")});
                 }
-
             }
-        });
-    }else {
-        client.lrange("latestsearches", -5, -1, function(error, result){
+        });*/
+    } else {
+        client.lrange("latestsearches", -5, -1, function (error, result) {
             console.log(arguments);
-            res.render('index', {title: 'Music comparer', latestsearches: result, authenticated: false, username: null});
+            res.render('index', {
+                title: 'Music comparer',
+                latestsearches: result,
+                authenticated: false,
+                username: null
+            });
         });
     }
-
-});
-
-router.post('/', function (req, res) {
-    console.log("Did we get here?");
-    spotify.search(req.body.query, req.body.type, function (result) {
-        result = JSON.parse(result);
-        console.log(typeof result);
-        res.render('index', {title: 'Music comparer', result: result});
-    });
 });
 
 module.exports = router;
