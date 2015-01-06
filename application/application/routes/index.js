b4a70c464da2e1afc409a5cf2eff324532f555a3
Christfoffer Holmgren <ch222kv@student.lnu.se>
2015-01-06 13:42:32 +0100

    Moved the rendering to another function to make it more DRY.

--- application/application/routes/index.js
+++ application/application/routes/index.js
@@ -8,6 +8,15 @@ var Q = require('q');
 var redis = require('redis'),
     client = redis.createClient();
 
+function render_index_page(title, latestsearches, authenticated, username){
+    res.render('index', {
+        title: title,
+        latestsearches: latestsearches,
+        authenticated: authenticated,
+        username: username
+    });
+}
+
 /* GET home page. */
 router.get('/', function (req, res) {
     console.log(req.cookies);
@@ -27,52 +36,29 @@ router.get('/', function (req, res) {
                 }
             }
         ).then(function (result) {
-                res.render('index', {
+                render_index_page('Music comparer', result, true, req.cookies.username);
+                /*res.render('index', {
                     title: 'Music comparer',
                     latestsearches: result,
                     authenticated: true,
                     username: req.cookies.username
-                });
+                });*/
             }).catch(function (error) {
                 console.log(error);
                 res.cookie("username", "", {expires: new Date(1), httpOnly: true});
                 res.cookie("hmac", "", {expires: new Date(1), httpOnly: true});
                 res.render("error", {error: new Error("Your cookies seems to be wrong, try to login again.")});
             });
-        /*client.get(req.cookies.username, function (error_username, result_username) {
-            if (error_username) {
-
-            } else {
-
-                var json = JSON.parse(result_username);
-                var hmac = crypto.createHash("md5").update(json.session.name + json.session.key, "utf8").digest("hex");
-                if (hmac === req.cookies.hmac) {
-                    client.lrange("latestsearches", -5, -1, function (error, result) {
-                        console.log(arguments);
-                        res.render('index', {
-                            title: 'Music comparer',
-                            latestsearches: result,
-                            authenticated: true,
-                            username: req.cookies.username
-                        });
-                    });
-                }
-                else {
-                    res.cookie("username", "", {expires: new Date(1), httpOnly: true});
-                    res.cookie("hmac", "", {expires: new Date(1), httpOnly: true});
-                    res.render("error", {error: new Error("Your cookies seems to be wrong, try to login again.")});
-                }
-            }
-        });*/
     } else {
         client.lrange("latestsearches", -5, -1, function (error, result) {
             console.log(arguments);
-            res.render('index', {
+            render_index_page('Music comparer', result, false, null);
+            /*res.render('index', {
                 title: 'Music comparer',
                 latestsearches: result,
                 authenticated: false,
                 username: null
-            });
+            });*/
         });
     }
 });
